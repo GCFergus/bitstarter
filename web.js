4c9a30c610a9d0d75df86493604ffbd0326480db
Geremy Ferguson <gcfergus@hotmail.com>
2013-07-11 06:03:03 +0000

    And again...

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ App.get('/', function(request, response) {
 
     var buffer = fs.readFileSync(index.html);
 
-    response.send(buffer.toString());
+    response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
