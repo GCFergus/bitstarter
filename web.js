31bf8f955c6a6b820134880d782ba7b91ec38db5
Geremy Ferguson <gcfergus@hotmail.com>
2013-07-11 05:13:35 +0000

    Updated web.js.

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
+App.get('/', function(request, response) {
 
     var buffer = fs.readFileSync(index.html);
     
