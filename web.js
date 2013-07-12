a2b5edde961c1ebd25ee1a4ed075bf4fcfef2023
Geremy Ferguson <gcfergus@hotmail.com>
2013-07-12 04:51:18 +0000

    Trying from new clone.

--- web.js
+++ web.js
@@ -1,17 +1,12 @@
 var express = require('express');
-var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-App.get('/', function(request, response) {
-	response.send('Please work.');
-
-
-//	response.send(fs.readFileSync('index.html').toString('utf8'));
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
-
 app.listen(port, function() {
-	console.log("Listening on " + port);
+  console.log("Listening on " + port);
 });
