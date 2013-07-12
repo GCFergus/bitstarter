fda0380cc15b273f19bd26401f2067f63d622bdf
Geremy Ferguson <gcfergus@hotmail.com>
2013-07-12 03:41:40 +0000

    Fixed issues with buffer usage.

--- web.js
+++ web.js
@@ -3,15 +3,19 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 App.get('/', function(request, response) {
+	var fs = require('fs');
 
-    var fs = require('fs');
+	var content = fs.readFileSync('index.html','utf8');
 
-    var buffer = fs.readFileSync('index.html');
+	var buffer = new Buffer(content,'utf8');
 
-    response.send(buffer.toString('utf-8'));
+	var bufferContent = buffer.toString('utf8');
+
+	response.send(bufferContent);
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
