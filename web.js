c31a2fdf6948be1dfd52656416b45f95052d56db
Geremy Ferguson <gcfergus@hotmail.com>
2013-07-12 04:18:36 +0000

    Desperation.

--- web.js
+++ web.js
@@ -4,11 +4,12 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 App.get('/', function(request, response) {
-	response.send(fs.readFileSync('./index.html').toString('utf8'));
+        response.send(fs.readFileSync('./index.html').toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
 
 app.listen(port, function() {
-	console.log("Listening on " + port);
+        console.log("Listening on " + port);
 });
+
