1077ff345f7cd0ae8056f1e3fccb824ffcb7169f
Geremy Ferguson <gcfergus@hotmail.com>
2013-07-12 04:58:34 +0000

    Add var contents

--- web.js
+++ web.js
@@ -4,10 +4,15 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	var contents = fs.readFileSync('index.html','utf8');
+
+
+
+
+	response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
