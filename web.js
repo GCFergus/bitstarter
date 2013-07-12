12875dd651eb3c70d72cdcb43fb7c890368c71e4
Geremy Ferguson <gcfergus@hotmail.com>
2013-07-12 04:30:59 +0000

    Removed and trying again.

--- web.js
+++ web.js
@@ -4,12 +4,14 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 App.get('/', function(request, response) {
-        response.send(fs.readFileSync('./index.html').toString('utf8'));
+	response.send('Please work.');
+
+
+//	response.send(fs.readFileSync('index.html').toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
 
 app.listen(port, function() {
-        console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
-
