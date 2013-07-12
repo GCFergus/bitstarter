7a7e949e198e1c0d1926a6a09785d642610ef778
Geremy Ferguson <gcfergus@hotmail.com>
2013-07-12 05:01:43 +0000

    Add all but response.send

--- web.js
+++ web.js
@@ -6,10 +6,13 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 	var contents = fs.readFileSync('index.html','utf8');
 
+	var buffer = new Buffer(contents,'utf8');
+
+	var bufferContents = buffer.ToString('utf8');
 
 
 
-	response.send('Hello World!');
+	response.send('Hello World 4!');
 });
 
 var port = process.env.PORT || 5000;
