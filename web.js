f3e3c969c5d0198ed3a25ddc2c8a413046cd6b4b
Geremy Ferguson <gcfergus@hotmail.com>
2013-07-12 05:12:12 +0000

    Simplified.

--- web.js
+++ web.js
@@ -4,13 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var contents = fs.readFileSync('index.html','utf8');
 
-	var buffer = new Buffer(contents,'utf8');
-
-	var bufferContents = buffer.ToString('utf8');
-
-
+	var buf = new Buffer(fs.readFileSync('index.html'));
 
 	response.send('Hello World 4!');
 });
